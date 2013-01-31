41a617901c7549d5ed5a5e64a66cde0a1810188f
Mateusz Korszun <mkorszun@gmail.com>
2013-01-31 15:37:05 +0100

    fixup

--- src/main/java/com/cloudcontrolled/sample/jetty/App.java
+++ src/main/java/com/cloudcontrolled/sample/jetty/App.java
@@ -1,38 +1,29 @@
 package com.cloudcontrolled.sample.jetty;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.http.*;
-
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.servlet.*;
+import org.eclipse.jetty.server.handler.ContextHandler;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 
 
-public class App extends HttpServlet
+public class App
 {
-    private static final long serialVersionUID = -1;
-    private static final String CSS = "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://www.cloudcontrol.com/documentation/Guides/helloworld.css\" media=\"screen\" />";
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException
-    {
-        resp.setContentType("text/html");
-        PrintWriter out = resp.getWriter();
-        out.print(CSS);
-        out.print("Hello world, I'm a Jetty app running on cloudControl!");
-        out.flush();
-        out.close();
-    }
-    
     public static void main(String[] args) throws Exception
     {
         Server server = new Server(Integer.valueOf(System.getenv("PORT")));
-        ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
-        context.setContextPath("/");
-        server.setHandler(context);
-        context.addServlet(new ServletHolder(new App()),"/");
+        ContextHandler context = new ContextHandler("/");
+        context.setHandler(new ResourceHandler());
+        context.setResourceBase("target/classes/template/hello.html");
+
+        ContextHandler resources = new ContextHandler("/static");
+        resources.setHandler(new ResourceHandler());
+        resources.setResourceBase("target/classes/static");
+
+        HandlerList handlers = new HandlerList();
+        handlers.setHandlers(new Handler[] {resources, context});
+        server.setHandler(handlers);
+
         server.start();
         server.join();
     }
